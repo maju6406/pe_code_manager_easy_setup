c3621a0a87393b1ed678e8338335ffab9ff74aa5
maju6406 <beersy@gmail.com>
2017-03-06 13:17:15 -0800

    Updating readme

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,13 +12,29 @@
 #
 #  r10k_remote_url: git url to your control repo, ex'git@gitlab:puppet/control-repo.git',
 #  git_management_system: 'gitlab' (Default) or 'github'
+#  gms_server_url: HTTP URL to your git repo, ex 'http://gitlab' (Ignore this param if you're using github.com)
+#  gms_api_token: Your github/gitlab personal access token (https://github.com/settings/tokens or http://davisgitlab/profile/personal_access_tokens)
+#  control_repo_project_name: Typically <owner>/<control_repo>, ex puppet/control-repo
 #
 # Examples
 # --------
 #
 # @example
 #    class { 'pe_code_manager_easy_setup':
-#      r10k_remote_url => 'git@gitlab:puppet/control-repo.git',
+#      r10k_remote_url => 'git@davisgitlab:puppet/control-repo.git',
+#      git_management_system => 'gitlab',
+#      gms_server_url=>'http://davisgitlab',
+#      control_repo_project_name => 'puppet/control-repo',
+#      gms_api_token => 'HkXboXkyKXQsWH9ZLAy9'
+#    }
+#
+#
+# @example
+#    class { 'pe_code_manager_easy_setup':
+#      r10k_remote_url => 'git@github.com:maju6406/thisisasamplerepotest.git',
+#      git_management_system => 'github',
+#      control_repo_project_name => 'maju6406/thisisasamplerepotest',
+#      gms_api_token => 'H3XboXkyKXQsWH9ZLAy8'
 #    }
 #
 # Output
@@ -38,11 +54,11 @@
 # Copyright 2017 Abir Majumdar, unless otherwise noted.
 #
 class pe_code_manager_easy_setup (
-  $r10k_remote_url = 'git@gitlab:puppet/control-repo.git',
-  $git_management_system = 'gitlab',
-  $gms_server_url = 'http://gitlab',
-  $gms_api_token = 'HkXboXkyKXQsWH9ZLAy9',
-  $control_repo_project_name = 'puppet/control-repo'
+  String $r10k_remote_url = 'git@gitlab:puppet/control-repo.git',
+  String $git_management_system = 'gitlab',
+  Optional[String]  $gms_server_url = 'http://gitlab',
+  String $gms_api_token = 'H3XboXkyKXQsWH9ZLAy8',
+  String $control_repo_project_name = 'puppet/control-repo'
 ){
 
     package { 'puppetclassify':
