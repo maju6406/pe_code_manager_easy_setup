be4eb3d17295ce90b31f029f6324dfb29e0da07f
maju6406 <beersy@gmail.com>
2018-01-09 17:03:24 -0800

    Fixing hopefully the last typo

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -2,7 +2,7 @@
 echo r10k_remote_url : $PT_r10k_remote_url
 echo git_management_system : $PT_git_management_system
 #puppet resource package puppetclassify ensure=present provider=puppet_gem
-puppet_command = "class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
+puppet_command = "class { \'pe_code_manager_easy_setup\': r10k_remote_url => \'$PT_r10k_remote_url\', git_management_system => \'$PT_git_management_system\'}"
 echo Running $puppet_command
 # puppet apply -e $puppet_command
 # puppet agent -t
