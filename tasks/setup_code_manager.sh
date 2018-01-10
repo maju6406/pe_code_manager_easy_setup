bd84714b38787e03ce8075871635482f4e4d7a17
maju6406 <beersy@gmail.com>
2018-01-10 13:07:59 -0800

    Checking exit codes

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -10,12 +10,15 @@ echo git_management_system : $PT_git_management_system
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
-echo "class { 'pe_code_manager_easy_setup': r10k_remote_url => 'https://github.com/maju6406/control-repo.git', git_management_system => 'gitlab'}"
+echo "class { 'pe_code_manager_easy_setup': r10k_remote_url => 'https://github.com/maju6406/control-repo.git', git_management_system => 'github'}"
 echo $puppet_command
 puppet apply --test -e "$puppet_command" 2>&1
+echo "Apply Exit code:$?" 
 echo "Running Puppet on Puppet Master..."
 puppet agent -t 2>&1
+echo "Run1 Exit code:$?"
 puppet agent -t 2>&1
+echo "Run2 Exit code:$?"
 echo "Done!"
 echo "Put this generated Public SSH Key in your version control system:"
 echo $(head -n 1 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub)
