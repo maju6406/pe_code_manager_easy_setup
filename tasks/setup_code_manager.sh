93b482a9251eb74dfed6a6bc70ae81a7e79099b4
maju6406 <beersy@gmail.com>
2018-01-09 17:45:17 -0800

    Uncommenting Puppet Commands

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -7,10 +7,9 @@ fi
 echo r10k_remote_url : $PT_r10k_remote_url
 echo git_management_system : $PT_git_management_system
 
-#puppet resource package puppetclassify ensure=present provider=puppet_gem
+puppet resource package puppetclassify ensure=present provider=puppet_gem
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
-printf '%s\n' "$puppet_command"
-# puppet apply -e $puppet_command
-# puppet agent -t
-# puppet agent -t
+puppet apply -e $puppet_command
+puppet agent -t
+puppet agent -t
 echo "Done!"
