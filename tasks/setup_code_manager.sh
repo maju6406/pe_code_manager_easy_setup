cea1c7bbbe576b7938eba05ccfaa5a13cfb6d29a
maju6406 <beersy@gmail.com>
2018-01-10 12:31:24 -0800

    Fixing broken puppet apply

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -10,7 +10,9 @@ echo git_management_system : $PT_git_management_system
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
-puppet apply --test -e $puppet_command 2>&1
+echo "class { 'pe_code_manager_easy_setup': r10k_remote_url => 'https://github.com/maju6406/control-repo.git', git_management_system => 'gitlab'}"
+echo $puppet_command
+puppet apply --test -e "$puppet_command" 2>&1
 echo "Running Puppet on Puppet Master..."
 puppet agent -t 2>&1
 puppet agent -t 2>&1
