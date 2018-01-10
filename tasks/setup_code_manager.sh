ce499f9025ec2bdc59f5ab93af2e966fc9bd8b8d
maju6406 <beersy@gmail.com>
2018-01-10 12:11:38 -0800

    Adding some debug

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -10,11 +10,12 @@ echo git_management_system : $PT_git_management_system
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
-puppet apply -e $puppet_command
+puppet apply --test -e $puppet_command 2>&1
 echo "Running Puppet on Puppet Master..."
-puppet agent -t
-puppet agent -t
+puppet agent -t 2>&1
+puppet agent -t 2>&1
 echo "Done!"
 echo "Put this generated Public SSH Key in your version control system:"
-cat /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub
+echo $(head -n 1 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub)
 echo "Put this webhook URL in your version control system:"
+echo $(head -n 1 /etc/puppetlabs/puppetserver/.puppetlabs/webhook_url.txt)
