687e81c9ac9915c91a4d249184eda9a2547f487d
maju6406 <beersy@gmail.com>
2018-01-09 17:41:49 -0800

    Adding some error checking

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+if facter -p puppet_server | grep -v -q `hostname`; then
+  echo "This task can only be run on the master node."; 
+  exit 1
+fi
+
 echo r10k_remote_url : $PT_r10k_remote_url
 echo git_management_system : $PT_git_management_system
+
 #puppet resource package puppetclassify ensure=present provider=puppet_gem
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
 printf '%s\n' "$puppet_command"
