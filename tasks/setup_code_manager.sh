b1a7826aaf8656539523d39837f8535b8e360f76
maju6406 <beersy@gmail.com>
2018-01-10 17:27:43 -0800

    Sending output to a file

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -10,13 +10,13 @@ echo git_management_system : $PT_git_management_system
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
 puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
-echo $puppet_command >/tmp/cm.pp
-puppet apply /tmp/cm.pp
+echo $puppet_command >/tmp/cm.pp 
+puppet apply /tmp/cm.pp 2>&1 >/tmp/ez_puppet_apply.log
 #echo "Apply Exit code:$?" 
 echo "Running Puppet on Puppet Master..."
-puppet agent -t
+puppet agent -t 2>&1> >/tmp/ez_puppet_agent_run1.log
 #echo "Run1 Exit code:$?"
-puppet agent -t
+puppet agent -t 2>&1 >/tmp/ez_puppet_agent_run2.log
 #echo "Run2 Exit code:$?"
 echo "Done!"
 echo "Put this generated Public SSH Key in your version control system:"
