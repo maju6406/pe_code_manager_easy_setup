aa98375dce281fe2224459b914325245262f7165
maju6406 <beersy@gmail.com>
2018-01-10 17:42:27 -0800

    Removing gms type param

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -3,13 +3,12 @@ if puppet config print server | grep -v -q `hostname`; then
   echo "This task can only be run on the master node."; 
   exit 1
 fi
-
-echo r10k_remote_url : $PT_r10k_remote_url
+PT_r10k_remote_url="github"
 echo git_management_system : $PT_git_management_system
 
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
-puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
+puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '', git_management_system => '$PT_git_management_system'}"
 echo $puppet_command >/tmp/cm.pp 
 puppet apply /tmp/cm.pp 2>&1 >/tmp/ez_puppet_apply.log
 #echo "Apply Exit code:$?" 
@@ -21,5 +20,10 @@ puppet agent -t 2>&1 >/tmp/ez_puppet_agent_run2.log
 echo "Done!"
 echo "Put this generated Public SSH Key in your version control system:"
 echo $(head -n 1 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub)
-echo "Put this webhook URL in your version control system:"
-echo $(head -n 1 /etc/puppetlabs/puppetserver/.puppetlabs/webhook_url.txt)
+echo "Example webhook URL's in your version control system:"
+webhook_url=$(head -n 1 /etc/puppetlabs/puppetserver/.puppetlabs/webhook_url.txt)
+echo "github:" ${webhook_url}
+echo "gitlab:" ${url/github/gitlab}
+echo "stash:" ${url/github/stash}
+echo "bitbucket:" ${url/github/bitbucket}
+echo "tfs-git:" ${url/github/tfs-git}
