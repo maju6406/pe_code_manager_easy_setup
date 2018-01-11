7c3a27831718b4aa247452157e6d527c31f8fc77
maju6406 <beersy@gmail.com>
2018-01-10 17:58:05 -0800

    Fixing some bugs

--- tasks/setup_code_manager.sh
+++ tasks/setup_code_manager.sh
@@ -8,14 +8,14 @@ echo git_management_system : $PT_git_management_system
 
 puppet resource package puppetclassify ensure=present provider=puppet_gem
 echo "Applying Changes to Puppet Master..."
-puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '', git_management_system => '$PT_git_management_system'}"
+puppet_command="class { 'pe_code_manager_easy_setup': r10k_remote_url => '$PT_r10k_remote_url', git_management_system => '$PT_git_management_system'}"
 echo $puppet_command >/tmp/cm.pp 
-puppet apply /tmp/cm.pp 2>&1 >/tmp/ez_puppet_apply.log
+puppet apply /tmp/cm.pp &>/tmp/ez_puppet_apply.log
 #echo "Apply Exit code:$?" 
 echo "Running Puppet on Puppet Master..."
-puppet agent -t 2>&1 >/tmp/ez_puppet_agent_run1.log
+puppet agent -t &>/tmp/ez_puppet_agent_run1.log
 #echo "Run1 Exit code:$?"
-puppet agent -t 2>&1 >/tmp/ez_puppet_agent_run2.log
+puppet agent -t &>/tmp/ez_puppet_agent_run2.log
 #echo "Run2 Exit code:$?"
 echo "Done!"
 echo "Put this generated Public SSH Key in your version control system:"
@@ -23,7 +23,7 @@ echo $(head -n 1 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub)
 echo "Example webhook URL's in your version control system:"
 webhook_url=$(head -n 1 /etc/puppetlabs/puppetserver/.puppetlabs/webhook_url.txt)
 echo "github:" ${webhook_url}
-echo "gitlab:" ${url/github/gitlab}
-echo "stash:" ${url/github/stash}
-echo "bitbucket:" ${url/github/bitbucket}
-echo "tfs-git:" ${url/github/tfs-git}
+echo "gitlab:" ${webhook_url/github/gitlab}
+echo "stash:" ${webhook_url/github/stash}
+echo "bitbucket:" ${webhook_url/github/bitbucket}
+echo "tfs-git:" ${webhook_url/github/tfs-git}
