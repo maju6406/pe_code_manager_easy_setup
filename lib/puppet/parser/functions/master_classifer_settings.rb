8618c258aa5f21c955a095f10e3e18125cdf6a58
maju6406 <beersy@gmail.com>
2018-01-10 18:05:25 -0800

    Tweaking ruby code

--- lib/puppet/parser/functions/master_classifer_settings.rb
+++ lib/puppet/parser/functions/master_classifer_settings.rb
@@ -1,8 +1,6 @@
 # Master Classifier Helper Function
 module Puppet::Parser::Functions
-  newfunction(:master_classifer_settings, type: :rvalue) do |args|
-# Ignore next line. args isn't used but is there for backwards compatibility
-    args = ""
+  newfunction(:master_classifer_settings, type: :rvalue) do |_args|
     function_parseyaml([function_file([File.join(lookupvar('settings::confdir').to_s, 'classifier.yaml')])])
   end
 end
